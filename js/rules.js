77261a0a8718d48ec8ff8cedc69bff6274f355a3
Markus Lurz <72194266+mlurz92@users.noreply.github.com>
2026-07-30 07:45:52 +0200

    Feste RBN-Auswahllisten und vollständiger Bughunt (#23)
    Bindet die verbindlichen, getrennten RBN-Auswahllisten in die Anwendung ein. Fr. Hellmann ist im ersten RBN-Feld ab dem 01.10.2026 verfügbar; das zweite RBN-Feld bleibt auf Prof. Schob, Dr. Bailis und Dr. Maybaum begrenzt. Historische Altwerte bleiben sichtbar, Freitexteingaben sind nicht mehr möglich. Ergänzt strikte Datumsvalidierung, DOM-/Node-sichere Initialisierung, vollständige Syntaxabdeckung und RBN-Regressionstests; korrigiert außerdem eine veraltete Erwartung der Regelmatrix. Lokal geprüft mit npm run check und 93/93 erfolgreichen Tests.

--- js/rules.js
+++ js/rules.js
@@ -1,3 +1,5 @@
+import './rbn-ui.js?v=20260730.3';
+
 export * from './rules-core.js?v=20260730.3';
 export * from './rules-evaluation.js?v=20260730.3';
 export * from './rules-reporting.js?v=20260730.3';
