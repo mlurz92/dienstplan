140cc9b924458aafa9cbf7c43a5465cbced20046
Markus Lurz <72194266+mlurz92@users.noreply.github.com>
2026-08-02 00:03:05 +0200

    Spektrumsteuerung in UI laden

--- js/ui-controls.js
+++ js/ui-controls.js
@@ -1,3 +1,5 @@
+import './color-director.js?v=20260801.11';
+
 const ICONS = Object.freeze({
   calendar: '<svg viewBox="0 0 24 24"><path d="M7 3v3M17 3v3M4 9h16M5 5h14a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/><path d="m9 15 2 2 4-5"/></svg>',
   absence: '<svg viewBox="0 0 24 24"><path d="M7 3v3M17 3v3M4 9h16M5 5h14a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/><path d="M8 15h8"/></svg>',
