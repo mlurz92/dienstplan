e226cc922de923c4b82ea91cf2c22fab1695df74
Markus Lurz <72194266+mlurz92@users.noreply.github.com>
2026-08-04 06:48:13 +0200

    Unify Auto-Plan runner cache token

--- js/auto-plan-runner.js
+++ js/auto-plan-runner.js
@@ -39,10 +39,10 @@
  * Anzeigestrang weiter.
  */
 
-import { buildAutoPlan } from './auto-planner.js?v=20260803.5';
-import { planProfileIds } from './auto-planner-engine.js?v=20260803.5';
+import { buildAutoPlan } from './auto-planner.js?v=20260803.4';
+import { planProfileIds } from './auto-planner-engine.js?v=20260803.4';
 
-const WORKER_URL = '/js/auto-plan-worker.js?v=20260803.5';
+const WORKER_URL = '/js/auto-plan-worker.js?v=20260803.4';
 
 export function createAutoPlanExecutionPlan({
   hardwareConcurrency = 2,
