0eac77a2cae8e2e9d143625fba6e8d6e2dec5eae
Markus Lurz <72194266+mlurz92@users.noreply.github.com>
2026-08-04 10:41:32 +0200

    Expose Worker bindings as global Env

--- workers/autoplan-v9/worker-configuration.d.ts
+++ workers/autoplan-v9/worker-configuration.d.ts
@@ -1,8 +1,12 @@
 import type { AutoPlanContainer, AutoPlanJob } from './src/index.js';
 
-interface Env {
-  AUTO_PLAN_JOBS: DurableObjectNamespace<AutoPlanJob>;
-  AUTO_PLAN_CONTAINER: DurableObjectNamespace<AutoPlanContainer>;
-  ENVIRONMENT: string;
-  SOLVER_TIMEOUT_SECONDS: string;
+declare global {
+  interface Env {
+    AUTO_PLAN_JOBS: DurableObjectNamespace<AutoPlanJob>;
+    AUTO_PLAN_CONTAINER: DurableObjectNamespace<AutoPlanContainer>;
+    ENVIRONMENT: string;
+    SOLVER_TIMEOUT_SECONDS: string;
+  }
 }
+
+export {};
